458020173a3c21a9ccab91a7af0dcaa9fe661a07
769839948 <769839948@qq.com>
2016-06-25 10:38:47 +0800

    test文件夹

---
+++ Company/Company/Home/HomeViewModel.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewModel.h
+//  Company
+//
+//  Created by Zhang on 6/25/16.
+//  Copyright © 2016 Zhang. All rights reserved.
+//
+
+#import "BaseRequestViewModel.h"
+
+@interface HomeViewModel : BaseRequestViewModel
+
+@end
---
+++ Company/Company/Message/MessageViewModel.m
@@ -0,0 +1,13 @@
+//
+//  MessageViewModel.m
+//  Company
+//
+//  Created by Zhang on 6/25/16.
+//  Copyright © 2016 Zhang. All rights reserved.
+//
+
+#import "MessageViewModel.h"
+
+@implementation MessageViewModel
+
+@end
